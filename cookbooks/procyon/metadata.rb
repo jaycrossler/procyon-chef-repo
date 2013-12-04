2c9776c9a971098eb13611445084a754fcc9bb7f
Tyler Garner <garnertb@gmail.com>
2013-12-03 20:35:14 -0500

    Add the nginx recipe, set cookbook dependencies, create the initial procyon recipe.

--- cookbooks/procyon/metadata.rb
+++ cookbooks/procyon/metadata.rb
@@ -6,3 +6,10 @@ description      'Installs/Configures procyon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "nginx"
+depends "java"
+depends "tomcat"
+depends "hostsfile"
+depends "postgresql"
+depends "database"
+
--- cookbooks/procyon/recipes/default.rb
+++ cookbooks/procyon/recipes/default.rb
@@ -1,8 +1,22 @@
-#
-# Cookbook Name:: procyon
-# Recipe:: default
-#
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
-#
+procyon_pkgs =  "build-essential python-dev".split
+
+procyon_pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+python_virtualenv node['procyon']['virtualenv']['location'] do
+  interpreter "python2.7"
+  action :create
+end
+
+git node['procyon']['location'] do
+  repository node['procyon']['git_repo']['location']
+  revision node['procyon']['git_repo']['branch']
+  action :sync
+end
+
+include_recipe 'procyon::nginx'
+
+

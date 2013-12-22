94e103a0ca592306ff1499422b8b7b0b77a7e46e
Jay Crossler <jay.crossler@gmail.com>
2013-12-22 13:39:06 -0500

    Update PIL recipe to 'relink' if the link already existed so as not to throw error

--- scripts/install_PIL.sh
+++ scripts/install_PIL.sh
@@ -2,6 +2,6 @@
 
  sudo apt-get build-dep python-imaging
 
- sudo ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib/
- sudo ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/
- sudo ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
+ sudo ln -s -f /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib/
+ sudo ln -s -f /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/
+ sudo ln -s -f /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
